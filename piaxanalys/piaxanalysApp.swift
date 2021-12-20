2193ad75fbf17f2b4c3feba1692cda8fb2518531
Bill Mårtensson <bill@magictechnology.se>
2021-12-20 09:10:28 +0100

    Initial Commit

---
+++ piaxanalys/piaxanalysApp.swift
@@ -0,0 +1,17 @@
+//
+//  piaxanalysApp.swift
+//  piaxanalys
+//
+//  Created by Bill Martensson on 2021-12-20.
+//
+
+import SwiftUI
+
+@main
+struct piaxanalysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ piaxanalysUITests/piaxanalysUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  piaxanalysUITestsLaunchTests.swift
+//  piaxanalysUITests
+//
+//  Created by Bill Martensson on 2021-12-20.
+//
+
+import XCTest
+
+class piaxanalysUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
